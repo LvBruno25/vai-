db41aa87f04c2eeb955b119818e7ea3abb21cd09
LvBruno25 <brunoaamorim99@gmail.com>
2025-04-16 15:30:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -144,8 +144,5 @@ git push origin main
 
 tokenizer = AutoTokenizer.from_pretrained("./finbert")
 model = AutoModelForSequenceClassification.from_pretrained("./finbert")
-git add finbert/ main.py
-git commit -m "feat: usar modelo FinBERT local"
-git push origin main
 
 
